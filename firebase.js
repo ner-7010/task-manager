fbfb8b7548fce2d14d14beb6f183a36c4c010421
Naoto_Kawajiri <naoto.2298@gmail.com>
2025-04-05 18:39:45 +0900

    feat: 初期化部分修正

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getAnalytics, isSupported } from "firebase/analytics"; // Analytics（�
 
 // Firebase 設定情報（Firebase Console から取得）
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Firebase を初期化
@@ -24,8 +24,13 @@ const auth = getAuth(app); // 認証サービス
 const db = getFirestore(app); // Firestore データベース
 
 let analytics;
-if (typeof window !== "undefined" && isSupported() && process.browser) {
-    analytics = getAnalytics(app); // クライアントサイドでのみ初期化
+
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
 }
 
 // 使いたいサービスをエクスポート
